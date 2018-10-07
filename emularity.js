03d19f43d47452a661a86411ecf65e3905819bd2
Pengan Zhou <pengan1987@msn.com>
2018-10-06 17:07:54 -0700

    SB486 and MacIIci

--- emularity.js
+++ emularity.js
@@ -1,6 +1,7 @@
 var emuRunner = null;
 var emuConfig = null;
 var machineConfig = null;
+var machineId = "emularity";
 
 function getUrlVars() {
     var vars = {};
@@ -12,10 +13,10 @@ function getUrlVars() {
 
 function processMachineJson(data) {
     var machineList = data.machines;
-    var machineid = getUrlVars()["machine"];
+    machineId = getUrlVars()["machine"];
     for (i = 0; i < machineList.length; i++) {
         var machine = machineList[i];
-        if (machine.id == machineid) {
+        if (machine.id == machineId) {
             processMachineConfig(machine);
         }
     }
@@ -34,7 +35,8 @@ function runDoxbox() {
                 return filename;
             }
         }),
-        PC98DosBoxLoader.startExe(machineConfig.startExe)
+        DosBoxLoader.startExe(machineConfig.startExe),
+        DosBoxLoader.fileSystemKey(machineId)
     ]
 
     for (var i = 0; i < machineConfig.zips.length; i++) {
@@ -60,7 +62,8 @@ function runPC98Dosbox() {
         PC98DosBoxLoader.emulatorJS(emuConfig.emulatorJS),
         PC98DosBoxLoader.emulatorWASM(emuConfig.emulatorWASM),
         PC98DosBoxLoader.nativeResolution(emuConfig.nativeResolution.width, emuConfig.nativeResolution.height),
-        PC98DosBoxLoader.startExe(machineConfig.startExe)
+        PC98DosBoxLoader.startExe(machineConfig.startExe),
+        PC98DosBoxLoader.fileSystemKey(machineId)
     ];
 
     if (emuConfig.mountFile) {
@@ -119,6 +122,79 @@ function runSAE() {
 
 function runMAME() {
     console.log("running mame");
+    var emuArguments = [
+        JSMAMELoader.driver(emuConfig.driverName),
+        JSMAMELoader.nativeResolution(emuConfig.nativeResolution.width, emuConfig.nativeResolution.height),
+        JSMAMELoader.emulatorJS(emuConfig.emulatorJS),
+        JSMAMELoader.emulatorWASM(emuConfig.emulatorWASM),
+    ];
+    if (emuConfig.mountFile) {
+        for (var i = 0; i < emuConfig.mountFile.length; i++) {
+            var file = emuConfig.mountFile[i];
+            emuArguments.push(
+                JSMAMELoader.mountFile(
+                    file.targetPath,
+                    JSMAMELoader.fetchFile(
+                        file.title,
+                        file.source
+                    ))
+            );
+        }
+    }
+    if (emuConfig.mountZip) {
+        for (var i = 0; i < emuConfig.mountZip.length; i++) {
+            var file = emuConfig.mountZip[i];
+            emuArguments.push(
+                JSMAMELoader.mountZip(
+                    file.targetPath,
+                    JSMAMELoader.fetchFile(
+                        file.title,
+                        file.source
+                    ))
+            );
+        }
+    }
+
+    for (var i = 0; i < machineConfig.files.length; i++) {
+        var file = machineConfig.files[i];
+        emuArguments.push(
+            JSMAMELoader.mountFile(
+                file.targetPath,
+                JSMAMELoader.fetchFile(
+                    file.title,
+                    file.source
+                ))
+        );
+    }
+
+    for (var i = 0; i < machineConfig.peripherals.length; i++) {
+        var peripheral = machineConfig.peripherals[i];
+        emuArguments.push(
+            JSMAMELoader.peripheral(peripheral.name, peripheral.value)
+        );
+    }
+
+    var extraArgs = [];
+    if (emuConfig.extraArgs) {
+        extraArgs = extraArgs.concat(emuConfig.extraArgs);
+    }
+    if (machineConfig.extraArgs) {
+        extraArgs = extraArgs.concat(machineConfig.extraArgs);
+    }
+
+    if (extraArgs.length > 0) {
+        emuArguments.push(
+            JSMAMELoader.extraArgs(extraArgs)
+        );
+    }
+
+    var canvas = document.querySelector("#emularity-canvas");
+    canvas.onclick = function () {
+        canvas.requestPointerLock();
+    }
+
+    var emulator = new Emulator(document.querySelector("#emularity-canvas"), null, JSMESSLoader.apply(this, emuArguments));
+    emulator.setScale(3).start({ waitAfterDownloading: true });
 }
 
 function processMachineConfig(machine) {

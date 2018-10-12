f3af2201f7194fb3f426f7e99080389994292cd1
Pengan Zhou <pengan1987@msn.com>
2018-10-11 22:45:11 -0700

    Add stat code, update thumbnail

---
+++ baidu-stat.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5b8bdeaa6797830f380679db085bdea6";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+

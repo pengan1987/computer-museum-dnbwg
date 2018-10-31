1d82077b8db85c0ff2ade4101b1b62213708d878
Pengan Zhou <pengan1987@msn.com>
2018-10-30 21:57:14 -0700

    Add Atari 2600

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function processJson(data) {
-    console.log(data);
     var machineList = data.machines;
     var base = $("#base-cell");
     var machineListContainer = $("#machine-list");
@@ -8,9 +7,12 @@ function processJson(data) {
         var clone = base.clone();
         var title = machine.name + " - " + machine.year;
         var playerlink = "player.html?machine=" + machine.id;
-		if (machine.emularity){
-			playerlink = "emularity.html?machine=" + machine.id;
-		}
+        if (machine.emularity) {
+            playerlink = "emularity.html?machine=" + machine.id;
+        }
+        if (machine.directurl) {
+            playerlink = machine.directurl;
+        }
         clone.show();
         clone.attr("id", machine.id);
         clone.find("a").attr("href", playerlink);
@@ -21,7 +23,6 @@ function processJson(data) {
 }
 
 $(document).ready(function () {
-    console.log("ready!");
     $.getJSON("machines.json", processJson);
 });
 
--- standalone/javatari.md
+++ standalone/javatari.md
@@ -1,1 +1,5 @@
-Atari 2600
+Atari 2600 模拟器，方向键控制方向，空格键开火，Del键连发
+
+[演示程序集](?ROM=http://dnbwg2.cdn.bcebos.com/javatari/SceneDemos6in1.bin)
+
+[星际堡垒（Star Castle）](?ROM=http://dnbwg2.cdn.bcebos.com/javatari/star_castle_homebrew.bin) 是一款2012年在Kickstarter上众筹成功的Atari 2600游戏

6f7bbf66ac33ce8c5e9f699b0a8b1610b0afecc9
Andy(Pengan) Zhou <pengan1987@msn.com>
2019-07-24 00:04:00 +0800

    Update baidu status token

--- baidu-stat.js
+++ baidu-stat.js
@@ -1,20 +1,7 @@
 var _hmt = _hmt || [];
-(function () {
+(function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?5b8bdeaa6797830f380679db085bdea6";
-  var s = document.getElementsByTagName("script")[0];
+  hm.src = "https://hm.baidu.com/hm.js?ec7f3937e62960acac7edcc856d00121";
+  var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
-})();
-
-(function () {
-  var bp = document.createElement('script');
-  var curProtocol = window.location.protocol.split(':')[0];
-  if (curProtocol === 'https') {
-    bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
-  }
-  else {
-    bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-  }
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(bp, s);
 })();

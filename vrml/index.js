f85be70825846969a8c6353d2b3e9df8adf8a24e
Andy(Pengan) Zhou <pengan1987@msn.com>
2020-06-28 13:07:21 -0700

    Add VRML mini exhibition

---
+++ vrml/index.js
@@ -0,0 +1,23 @@
+function processJson(data) {
+    var machineList = data.machines;
+    var base = $("#base-cell");
+    var machineListContainer = $("#machine-list");
+    for (i = 0; i < machineList.length; i++) {
+        var machine = machineList[i];
+        var clone = base.clone();
+        var title = machine.name + " - " + machine.year;
+        var playerlink = "xite.html?machine=" + machine.id;
+        clone.show();
+        clone.attr("id", machine.id);
+        clone.find("a").attr("href", playerlink);
+        clone.find("figcaption").text(title)
+        clone.find(".figure-img").attr("src", machine.image)
+        machineListContainer.append(clone);
+    }
+}
+
+$(document).ready(function () {
+    $.ajaxSetup({ cache: false });
+    $.getJSON("machines.json", processJson);
+});
+
---
+++ vrml/xite.js
@@ -0,0 +1,70 @@
+
+var machineId = "emularity";
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function processMachineJson(data) {
+    var machineList = data.machines;
+    machineId = getUrlVars()["machine"];
+    for (i = 0; i < machineList.length; i++) {
+        var machine = machineList[i];
+        if (machine.id == machineId) {
+            processMachineConfig(machine);
+        }
+    }
+}
+
+function processMachineConfig(machine) {
+    console.log(machine);
+    var x3dbrowser = X3D.getBrowser("#xiteview");
+    if (x3dbrowser) {
+        x3dbrowser.addBrowserCallback(X3D.X3DConstants.INITIALIZED_EVENT, function (good) {
+            console.log(good);
+            x3dbrowser.setBrowserOption("Gravity", 0);
+        });
+    }
+    $("#xiteview").attr("src", machine.url);
+
+    showIntroduction(machine.introduction);
+}
+
+$(document).ready(function () {
+    $.ajaxSetup({ cache: false });
+
+    var machineParam = getUrlVars()["machine"];
+    var emularityParam = getUrlVars()["emularity"];
+    var machineUrlParam = getUrlVars()["machineurl"];
+
+    if (machineParam && machineParam.length > 0) {
+        $.getJSON("machines.json", processMachineJson);
+    } else if (emularityParam && machineUrlParam.length > 0) {
+        machineId = emularityParam + "-" + machineUrlParam.replace(/[.:#\/\\]/g, '-');
+        var dummyMachineConfig = {
+            "machineId": machineId,
+            "emularity": emularityParam,
+            "url": machineUrlParam
+        }
+        console.log(dummyMachineConfig);
+        processMachineConfig(dummyMachineConfig);
+    }
+});
+
+function showIntroduction(introUrl) {
+    if (!introUrl || introUrl == "") {
+        introUrl = "../document/pending.md";
+    }
+    var showdownConv = new showdown.Converter();
+    showdownConv.setOption('tables', true);
+    $.get(introUrl,
+        function (data) {
+            var htmlContent = showdownConv.makeHtml(data);
+            $("#introduction").html(htmlContent);
+        }
+    );
+}

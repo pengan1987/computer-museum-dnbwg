ef5686d392765b1cf01962700ee1354b8dec6cfe
Andy(Pengan) Zhou <pengan1987@msn.com>
2025-06-22 21:17:54 +0800

    Update for Shanzhaiji

---
+++ standalone/shanzhaiji/details.js
@@ -0,0 +1,68 @@
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function processMachineJson(data) {
+    var machineList = data.machines;
+    machineId = getUrlVars()["machine"];
+    if (!machineId) {
+        machineId = 14852;
+    }
+    for (i = 0; i < machineList.length; i++) {
+        var machine = machineList[i];
+        if (machine.model_list_page_id == machineId) {
+            processMachineConfig(machine);
+        }
+    }
+}
+
+function processMachineConfig(machine) {
+    if (machine.valid_imgs_image) {
+        $("#valid_imgs_image").attr('src', '//famicn-1255835060.file.myqcloud.com/shanzhaiji/images/'+machine.valid_imgs_image);
+        $("#valid_imgs_image").show();
+    }
+
+    $("#model_details_name").text(machine.model_details_name);
+    $("#long_name").text(machine.long_name);
+    $("#price").text(machine.price);
+    $("#network").text(machine.network);
+    $("#date").text(machine.date);
+    $("#size").text(machine.size);
+    $("#weight").text(machine.weight);
+    $("#accessories").text(machine.accessories);
+    $("#standby_time").text(machine.standby_time);
+    $("#call_time").text(machine.call_time);
+    $("#language").text(machine.language);
+    $("#display").text(machine.display);
+    $("#ringtone").text(machine.ringtone);
+    $("#audio_playback").text(machine.audio_playback);
+    $("#video_playback").text(machine.video_playback);
+    $("#camera").text(machine.camera);
+    $("#connectivity").text(machine.connectivity);
+    $("#image_format").text(machine.image_format);
+    $("#app_platform").text(machine.app_platform);
+    $("#namecard").text(machine.namecard);
+    $("#power_manager").text(machine.power_manager);
+    $("#msg").text(machine.msg);
+    $("#clock_alarm").text(machine.clock_alarm);
+    $("#builtin_game").text(machine.builtin_game);
+    $("#os").text(machine.os);
+    $("#wifi").text(machine.wifi);
+    $("#java").text(machine.java);
+    $("#cpu").text(machine.cpu);
+    $("#memory").text(machine.memory);
+    $("#battery").text(machine.battery);
+    $("#functions").text(machine.functions);
+}
+
+$(document).ready(function () {
+    $.ajaxSetup({ cache: false });
+
+
+    $.getJSON("shanzhaiji.json", processMachineJson);
+
+});
---
+++ standalone/shanzhaiji/index.js
@@ -0,0 +1,110 @@
+var machineList;
+var newMachineList = [];
+var pages = [];
+var loadedPage = 0;
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function processJson(data) {
+    if (data.machines) {
+        data = data.machines;
+    }
+    machineList = data;
+
+    reorderedList = splitArrayByTime(machineList);
+
+    var i, j, temparray, chunk = 32;
+    for (i = 0, j = reorderedList.length; i < j; i += chunk) {
+        temparray = reorderedList.slice(i, i + chunk);
+        pages.push(temparray);
+    }
+    showMachines(pages[loadedPage]);
+}
+
+function loadmore() {
+    showMachines(pages[loadedPage]);
+}
+
+function showMachines(machines) {
+    var base = $("#base-cell");
+    var machineListContainer = $("#machine-list");
+    for (i = 0; i < machines.length; i++) {
+        var machine = machines[i];
+        var clone = base.clone();
+        var title = machine.model_list_name;
+        var playerlink =  "details.html?machine=" + machine.model_list_page_id;
+
+
+
+        var imageLink = "logo.gif";
+        if (machine.valid_imgs_image) {
+            imageLink = "//famicn-1255835060.file.myqcloud.com/shanzhaiji/thumbs/" + machine.valid_imgs_image;
+            //imageLink = imageLink.replace("{{image-path-new}}", imagePathNew);
+        }
+
+
+        clone.show();
+        clone.attr("id", machine.id);
+        clone.find("a").attr("href", playerlink);
+        clone.find("figcaption").text(title);
+        clone.find(".figure-img").attr("src", imageLink);
+        clone.addClass("show-data");
+        machineListContainer.append(clone);
+    }
+    loadedPage++;
+    if (loadedPage >= pages.length) {
+        $("#loadmore").hide();
+    }
+}
+
+function search() {
+
+    var keyword = $("#search-text").val();
+    if (!keyword || keyword.length == 0) {
+        $(".show-data").remove();
+        loadedPage = 0;
+        processJson(machineList);
+        return;
+    }
+    var searchResult = [];
+    for (i = 0; i < machineList.length; i++) {
+        var text = machineList[i].model_list_name;
+        text = text.toLowerCase();
+        keyword = keyword.toLowerCase();
+        if (text.includes(keyword)) {
+            searchResult.push(machineList[i]);
+        }
+    }
+    pages = [];
+    loadedPage = 0;
+    $(".show-data").remove();
+    showMachines(searchResult);
+}
+
+function splitArrayByTime(someArray) {
+    var date = new Date();
+    var timestamp = date.getTime();
+    var timetoken = Math.round(timestamp / (600 * 1000));
+    var position = timetoken % someArray.length;
+    console.log(position);
+    var first = someArray.slice(0, position);
+    var second = someArray.slice(position);
+    return second.concat(first);
+}
+
+$(document).ready(function () {
+
+    $.getJSON("shanzhaiji.json", processJson);
+});
+
+$(window).scroll(function () {
+    if ($(document).height() - $(window).height() - $(window).scrollTop() < 50 && loadedPage < pages.length) {
+        showMachines(pages[loadedPage]);
+    }
+});
